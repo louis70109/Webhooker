4e0010d16df58218e8cfbbf6f36887389cd43d2f
NiJia <louis70109@gmail.com>
2020-10-10 14:14:27 +0800

    modify to github image url

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ async function testAction(context) {
     hero: {
       type: 'image',
       url:
-        'https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png',
+        'https://raw.githubusercontent.com/louis70109/Webhooker/master/sample.png',
       size: 'full',
       aspectRatio: '20:13',
       aspectMode: 'cover',
